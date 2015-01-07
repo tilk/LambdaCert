fdf18a6cf4964645d610f6f8cff39892761a7197
Marek Materzok <marek.materzok@inria.fr>
2015-01-07 15:37:55 +0100

    Added desugaring through external LambdaS5.

--- LambdaS5/caml/eval.ml
+++ LambdaS5/caml/eval.ml
@@ -5,31 +5,43 @@ let usage_msg = "Usage: "
 
 let store = ref None
 
+let js_parser = ref None 
+
 let get_store () = if Option.is_none !store then store := Some Store.create_store; Option.get !store
 
 let get_channel filename =
     if filename = "stdin" then stdin else open_in filename
 
 let handle_parameter filename = 
-    let ch = get_channel filename in
-    Run.print_result (Run.eval_ast (get_store ()) (Run.parse_es5 ch filename)); 
-    close_in ch
+    let ast = match !js_parser with
+        | Some jp -> (* assuming inputs to be JS files *)
+            jp filename
+        | None -> (* assuming inputs to be LJS files *)
+            let ch = get_channel filename in
+            let ret = Run.parse_es5 ch filename in
+            close_in ch; ret in
+    Run.print_result (Run.eval_ast (get_store ()) ast)
 
 let load_store filename = 
-    let ch = open_in filename in 
-    store := Some (Marshal.from_channel (open_in filename)); 
-    close_in ch
+    File.with_file_in filename (fun ch -> store := Some (Marshal.from_channel (open_in filename)))
 
 let save_store filename = 
-    let ch = open_out filename in 
-    Marshal.to_channel ch (get_store ()) [Marshal.Closures]; 
-    close_out ch
+    File.with_file_out filename (fun ch -> Marshal.to_channel ch (get_store ()) [Marshal.Closures])
 
 let load_env filename = 
     let ch = get_channel filename in 
     let (st, _res) = Run.eval_ast (get_store ()) (Run.parse_es5_env ch filename (Syntax.String (CoqUtils.explode "dumped"))) in
     store := Some st;
     close_in ch
+
+let s5_js_parser filename jsfile = 
+    let s5out = Unix.open_process_in (filename ^ " -desugar " ^ Filename.quote jsfile ^ " -print-src") in
+    let s5ret = Run.parse_es5 s5out jsfile in
+    match Unix.close_process_in s5out with
+        | Unix.WEXITED 0 -> s5ret
+        | _ -> failwith "S5 desugar failure"
+
+let desugar_s5 filename = js_parser := Some (s5_js_parser filename)
 
 let _ =
     Arg.parse 
@@ -41,7 +53,10 @@ let _ =
          "save the environment in a file";
          "-env",
          Arg.String load_env,
-         "environment to parse"
+         "environment to parse";
+         "-desugarS5",
+         Arg.String desugar_s5,
+         "path to LambdaS5 (for desugaring js)"
         ] 
         handle_parameter
         usage_msg
